acdb8ef6a8a15b6b6c965a3c4a9500bdb007a88f
RicardoHernandezVarela <42160319+RicardoHernandezVarela@users.noreply.github.com>
2019-08-29 16:26:47 -0500

    POST modified

--- ecg/static/ecg/js/post.js
+++ ecg/static/ecg/js/post.js
@@ -3,14 +3,11 @@ const enviar = document.getElementById('enviar');
 const respuesta = document.getElementById('respuesta');
 
 let pathname = window.location.pathname;
-let id = pathname.replace('/se%C3%B1ales/nuevoregistro/','');
-let url = "/señales/info/" + id + "/"
-let url2 = "/señales/rt/" + id + "/"
+let id = pathname.replace('/senales/nuevoregistro/','');
+let url = "/senales/info/" + id + "/"
+let url2 = "/senales/rt/" + id + "/"
 
 guardar.addEventListener('click', () => {
-    //console.log(pathname);
-    //console.log(id);
-    //console.log(url);
 
     var formData = new FormData();
     formData.append('mediciones', mediciones);
@@ -19,16 +16,19 @@ guardar.addEventListener('click', () => {
         url: url,
         type:'POST',
         data: formData,
+
+        success: function (data){
+            guardar.innerHTML = "Guardado";
+            guardar.style.backgroundColor = "#9e9e9e";
+        },
+
         processData: false,
         contentType: false
 
     });
 
-    guardar.innerHTML = "Guardado";
-    guardar.style.backgroundColor = "#9e9e9e";
-
-
   });
+
 
 /*Prueba para enviar y procesar datos en tiempo real*/
   enviar.addEventListener('click', () => {
